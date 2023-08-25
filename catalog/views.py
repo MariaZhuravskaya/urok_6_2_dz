8432433443764c0aef73473ad17f567db088581c
Maria <maria.mandrykina@gmail.com>
2023-08-25 20:16:27 +0300

    commit

---
+++ catalog/views.py
@@ -0,0 +1,21 @@
+import json
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, 'catalog/index.html')
+
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f"{name}, {phone},{message}")
+    return render(request, 'catalog/contact.html')
+
+
+
+# Create your views here.
